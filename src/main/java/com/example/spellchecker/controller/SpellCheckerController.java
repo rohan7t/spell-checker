6f44287627a6053ef3544676ce5dbb28f84cef37
Rohan Tigadi <rohan5393@gmail.com>
2019-03-07 16:08:51 -0500

    Initial maven project structure

---
+++ src/main/java/com/example/spellchecker/controller/SpellCheckerController.java
@@ -0,0 +1,41 @@
+package com.example.spellchecker.controller;
+
+import java.util.HashSet;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spellchecker.handler.SpellCheckerHandler;
+
+@RestController
+public class SpellCheckerController {
+
+    private static final String ERROR_OCCURRED = "An error occurred. Please try again\n";
+
+    @Autowired
+    SpellCheckerHandler spellChekerHandler;
+
+    @PostMapping("/loadDictionary")
+    @ResponseBody
+    public HashSet<String> loadDictionary(@RequestParam String path) {
+	if (path == null || path.length() == 0) {
+//	    return new HttpClientErrorException(ERROR_OCCURRED);
+	    return null;
+	}
+	return spellChekerHandler.fetchDictionaryWords(path);
+    }
+
+    @PostMapping("/spellCheck")
+    public List<String> spellCheck(@RequestParam String path) {
+	if (path == null || path.length() == 0) {
+//	    return new HttpClientErrorException(ERROR_OCCURRED);
+	    return null;
+	}
+	return spellChekerHandler.spellCheck(path);
+    }
+
+}
